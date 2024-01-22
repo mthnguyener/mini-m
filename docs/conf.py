d385b8a8c6c0fbfc701216f07d37a53fb6d6f7d7
mthnguyener <>
2024-01-21 19:54:12 -0500

    adding bedrock tutorial notebook

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../minim/'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'minim'
+project = 'mini-M'
 copyright = '2024, Minh Nguyen'
 author = 'Minh Nguyen'
 
